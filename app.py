46473d7d98aa79e5ff915723e2a2a9e17c0959ab
Riadh <pigerous@gmail.com>
2022-01-28 12:12:06 +0100

    Last

--- app.py
+++ app.py
@@ -12,30 +12,80 @@ from flask import Flask, request, jsonify, make_response # type: ignore
 app = Flask(__name__)
 api = Api(app,
         version='1.0',
-        title='ML in production',
-        description='Deployment_Project.',
+        title='White Wine Prediction',
+        description='Jedha ML production project.',
 )
 
 
+@api.route('/fetchdata', methods=["POST"])
+class FetchData(Resource):
+    """ fetch data endpoint
+    """
+
+    @classmethod
+    def post(cls) :
+        """.
+        """
+        # Check if request has a JSON content
+        if request.json:
+            # Get the JSON as dictionnary
+            req = request.get_json()
+            print(f"request : {req}")
+            print(req.keys())
+
+            # Check mandatory key
+            if "column" in req.keys():
+                # col
+                col = req["column"]
+
+                # read data
+                data = pd.read_csv("./data/winequality.csv")
+                print(type(data))
+
+                if col in data.columns :
+                    # pylint: disable=E1136
+                    data = data[col].to_list()
+                    data_json = {col: data}
+
+                    return make_response(jsonify(data_json), 200)
+
+                return jsonify({"msg": "Error : col to fetch not in dataset champs!"})
+
+            return jsonify({"msg": "Error : column needed in json post files!"})
+        return jsonify({"msg": "Error: not a JSON or no email key in your request"})
 
 
 @api.route('/predict', methods=["POST"])
-def index():
-    # Check if request has a JSON content
-    if request.json:
-        # Get the JSON as dictionnary
-        req = request.get_json()
-        # Check mandatory key
-        if "input" in req.keys():
-            # Load model
-            classifier = joblib.load("./model/model.joblib")
-            # Predict
-            prediction = classifier.predict([req["input"]])
-            # Return the result as JSON but first we need to transform the
-            # result so as to be serializable by jsonify()
-            prediction = str(prediction[0])
-            return jsonify({"predict": prediction}), 200
-    return jsonify({"msg": "Error: not a JSON key in your request"})
+class Predict(Resource):
+    """ predict endpoint.
+    """
+
+    @classmethod
+    def post(cls):
+        """.
+        """
+        # Check if request has a JSON content
+        if request.json:
+            # Get the JSON as dictionnary
+            req = request.get_json()
+            print(f"request : {req}")
+            print(req.keys())
+
+            # Check mandatory key
+            if "input" in req.keys():
+                # Load model
+                model_file = os.path.join("./model/model.joblib")
+                reg = joblib.load(model_file)
+
+                # Predict
+                prediction = reg.predict(req["input"])
+                prediction = prediction.tolist()
+
+                # return prediction
+                # return jsonify({"predict": prediction}), 200
+                data = {"predict": prediction}
+                return make_response(jsonify(data), 200)
+        return jsonify({"msg": "Error: not a JSON or no email key in your request"})
 
 
 if __name__ == "__main__":
