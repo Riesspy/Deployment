55f2062cae2a6cb9aaf213baf3af708023d3a58a
Riadh <pigerous@gmail.com>
2022-01-28 12:31:42 +0100

    Laste_2

--- app.py
+++ app.py
@@ -1,70 +1,25 @@
-"""
-docstring to make pylint happy.
-"""
+
 
 import os
-import joblib # type: ignore
-import pandas as pd # type: ignore
-from flask_restx import Resource, Api # type: ignore
-from flask import Flask, request, jsonify, make_response # type: ignore
+import joblib
+import pandas as pd
+from flask_restx import Resource, Api 
+from flask import Flask, request, jsonify, make_response 
 
 
 app = Flask(__name__)
 api = Api(app,
-        version='1.0',
-        title='White Wine Prediction',
-        description='Jedha ML production project.',
+        title='Wine-o-meter',
+        description='ML in production',
 )
 
-
-@api.route('/fetchdata', methods=["POST"])
-class FetchData(Resource):
-    """ fetch data endpoint
-    """
-
-    @classmethod
-    def post(cls) :
-        """.
-        """
-        # Check if request has a JSON content
-        if request.json:
-            # Get the JSON as dictionnary
-            req = request.get_json()
-            print(f"request : {req}")
-            print(req.keys())
-
-            # Check mandatory key
-            if "column" in req.keys():
-                # col
-                col = req["column"]
-
-                # read data
-                data = pd.read_csv("./data/winequality.csv")
-                print(type(data))
-
-                if col in data.columns :
-                    # pylint: disable=E1136
-                    data = data[col].to_list()
-                    data_json = {col: data}
-
-                    return make_response(jsonify(data_json), 200)
-
-                return jsonify({"msg": "Error : col to fetch not in dataset champs!"})
-
-            return jsonify({"msg": "Error : column needed in json post files!"})
-        return jsonify({"msg": "Error: not a JSON or no email key in your request"})
 
 
 @api.route('/predict', methods=["POST"])
 class Predict(Resource):
-    """ predict endpoint.
-    """
-
     @classmethod
     def post(cls):
-        """.
-        """
-        # Check if request has a JSON content
+         # Check if request has a JSON content
         if request.json:
             # Get the JSON as dictionnary
             req = request.get_json()
@@ -85,7 +40,7 @@ class Predict(Resource):
                 # return jsonify({"predict": prediction}), 200
                 data = {"predict": prediction}
                 return make_response(jsonify(data), 200)
-        return jsonify({"msg": "Error: not a JSON or no email key in your request"})
+        return jsonify({"msg": "Error: not a JSON in your request"})
 
 
 if __name__ == "__main__":
